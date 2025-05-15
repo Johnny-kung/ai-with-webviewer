7731d7e784d45c0e0c5ebede820307f9fc153dd5
Johnny Kung <jkung@pdftron.com>
2025-05-15 13:03:49 -0700

    removed unused code

--- app/src/core/openAIService.js
+++ app/src/core/openAIService.js
@@ -1,15 +1,3 @@
-const WEBVIEWER_SUPPORT_PRE_PROMPT = `Any story you tell must be a scary story.`;
-
-async function queryOpenAI(input) {
-  const response = await window.openAIClient.responses.create({
-    instructions: WEBVIEWER_SUPPORT_PRE_PROMPT,
-    model: 'gpt-4o-mini',
-    input: input,
-  });
-
-  return response;
-}
-
 async function streamOpenAI(input) {
   const { openai, preprompt } = window.openAIClient;
   const stream = await openai.chat.completions.create({
@@ -24,4 +12,4 @@ async function streamOpenAI(input) {
   return stream;
 }
 
-export { queryOpenAI, streamOpenAI };
+export { streamOpenAI };
--- app/src/ui/chatbox.jsx
+++ app/src/ui/chatbox.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { queryOpenAI, streamOpenAI } from '../core/openAIService';
+import { streamOpenAI } from '../core/openAIService';
 import './chatbox.css';
 import IncomingMessage from './incomingMessage';
 
